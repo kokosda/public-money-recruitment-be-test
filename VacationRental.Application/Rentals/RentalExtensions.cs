df372183a821a1eb15969420c417a4409e8a159c
Victor Ponamarev <vponamrev@gmail.com>
2021-06-10 12:41:48 +0300

    RentalExtensions: converting Rental to RentalDto.

---
+++ VacationRental.Application/Rentals/RentalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VacationRental.Domain.Rentals;
+
+namespace VacationRental.Application.Rentals
+{
+    public static class RentalExtensions
+    {
+        public static RentalDto ToDto(this Rental rental)
+        {
+            if (rental == null) 
+                throw new ArgumentNullException(nameof(rental));
+            
+            var result = new RentalDto
+            {
+                Id = rental.Id,
+                Units = rental.Units
+            };
+            return result;
+        }
+    }
+}
